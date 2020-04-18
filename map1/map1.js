2b7559b3b622602e971e74dca30847194a493b06
Aline M Vandenbold <avand19@lsu.edu>
2020-04-17 19:55:57 -0500

    Help!

--- map1/map1.js
+++ map1/map1.js
@@ -1,10 +1,10 @@
-var OpenStreetMap = L.map('Chicago').setView([41.87, 87.62], 4)
+var OpenStreetMap = L.map('Beetlejuice').setView([41.87, 87.62], 6)
 L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(OpenStreetMap)
-var renameThisMarkerObject = L.marker([41.8863, 87.7173]).addTo(OpenStreetMap)
-var renameThisPolygonObject = L.polygon([
+var Park = L.marker([41.8863, 87.7173]).addTo(OpenStreetMap)
+var Greenway = L.polygon([
   [42, 85],
   [46, 85],
   [43, 79]
 ]).addTo(OpenStreetMap)
-renameThisPolygonObject.bindPopup('Chicago is boring and windy.')
-renameThisMarkerObject.bindPopup('Chicago is boring and windy.')
+Greenway.bindPopup('Chicago is boring and windy.')
+Park.bindPopup('Chicago is cool and funky.')
