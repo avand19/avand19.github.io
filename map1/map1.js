6a58ff332f5f2ec3bfab3fff7e7fafb4b686182a
Aline M Vandenbold <avand19@lsu.edu>
2020-04-18 12:47:47 -0500

    Lab 8.1

--- map1/map1.js
+++ map1/map1.js
@@ -1,7 +1,7 @@
-var OpenStreetMap = L.map('Beetlejuice').setView([41.87, 87.62], 6)
+ var OpenStreetMap = L.map('Beetlejuice').setView([41.87, 87.62], 6)
 L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(OpenStreetMap)
 var Park = L.marker([41.8863, 87.7173]).addTo(OpenStreetMap)
-var Greenway = L.polygon([
+var Greenway = L.polygon(
   [42, 85],
   [46, 85],
   [43, 79]
