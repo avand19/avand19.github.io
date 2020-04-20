c10f48af3547254da50f09fae01efa129d1195f4
Aline M Vandenbold <avand19@lsu.edu>
2020-04-19 22:14:50 -0500

    Night.3

--- Map2/map2.js
+++ Map2/map2.js
@@ -1,4 +1,4 @@
-var OpenStreetMap = L.map('Beetlejuice').setView([41.87, 87.62], 6)
+var OpenStreetMap = L.map('illinois').setView([41.87, -87.62], 6)
 L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(OpenStreetMap)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
@@ -7,8 +7,5 @@ L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   attribution: 'NOAA, Iowa State University'
 }).addTo(OpenStreetMap)
 L.tileLayer.wms('https://mesonet.argon.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-  layers: 'warnings_c',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
+  layers: 'warnings_c'
 }).addTo(OpenStreetMap)
