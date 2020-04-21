8e1baafa28a9224e4ec8717c2e5560fcd6ee1841
Aline M Vandenbold <avand19@lsu.edu>
2020-04-21 08:52:01 -0500

    Map3.2

--- Map3/map3.js
+++ Map3/map3.js
@@ -1,7 +1,19 @@
 /*gLobal L jQuery */
 var OpenStreetMap = L.map('map3').setView([41.87, -87.62], 6)
-L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(OpenStreetMap)
-var statesUrl = 'https://{s}.tile.opentopomap.org/{z}/{y}.png'
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{y}.png').addTo(OpenStreetMap)
+var statesUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(statesUrl, function (data) {
-  L.geoJSON(data, { style: { color: 'green' } }).addTo(OpenStreetMap)
-})
+ var stateStyle = function (feature) {
+   var pop = feature.properties.POPULATION
+   var statecolor = 'olive'
+   if (age < 38) { statecolor = 'green'}
+   return {
+   color: statecolor,
+   weight: 1,
+   fillOpacity: 0.2
+  }
+}
+  var geojsonoptions = {
+      style: stateStyle
+}
+  L.geoJSON(data, { style: statestyle }).addTo(OpenStreetMap)
