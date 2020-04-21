2cd9a07af57d67011caa3fa3f2283fad4e12e11b
Aline M Vandenbold <avand19@lsu.edu>
2020-04-21 09:35:08 -0500

    NotBlankpage?

--- Map3/map3.js
+++ Map3/map3.js
@@ -1,7 +1,13 @@
 /*gLobal L jQuery */
 var OpenStreetMap = L.map('map3').setView([41.87, -87.62], 6)
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{y}.png').addTo(OpenStreetMap)
+var statesLayerObject = L.layerGroup('https://{s}.tile.opentopomap.org/{z}/{y}.png').addTo(OpenStreetMap)
+var grayBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(renameThisMapObject)
 var statesUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
+var streetsBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(renameThisMapObject)
+var basemapsObject = {
+  'Streets': streetsBasemapObject,
+  'Gray canvas': grayBasemapObject
+}
 jQuery.getJSON(statesUrl, function (data) {
  var stateStyle = function (feature) {
    var pop = feature.properties.POPULATION
