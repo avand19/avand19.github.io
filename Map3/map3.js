882bbc2e01a62f7314c6d312cd710dace4d4f823
Aline M Vandenbold <avand19@lsu.edu>
2020-04-21 07:58:07 -0500

    map3.1

--- Map3/map3.js
+++ Map3/map3.js
@@ -1,11 +1,7 @@
-var OpenStreetMap = L.map('Beetlejuice').setView([41.87, 87.62], 6)
+/*gLobal L jQuery */
+var OpenStreetMap = L.map('map3').setView([41.87, -87.62], 6)
 L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(OpenStreetMap)
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
-}).addTo(OpenStreetMap)
-L.tileLayer.wms('https://mesonet.argon.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-  layers: 'warnings_c'
-}).addTo(OpenStreetMap)
+var statesUrl = 'https://{s}.tile.opentopomap.org/{z}/{y}.png'
+jQuery.getJSON(statesUrl, function (data) {
+  L.geoJSON(data, { style: { color: 'green' } }).addTo(OpenStreetMap)
+})
