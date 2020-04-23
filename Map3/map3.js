2aeee2577e87cd3a0b85c0cc485d124efa7d6177
Aline M Vandenbold <avand19@lsu.edu>
2020-04-23 08:36:08 -0500

    1234?

--- Map3/map3.js
+++ Map3/map3.js
@@ -1,9 +1,10 @@
 /*gLobal L jQuery */
-var OpenStreetMap = L.map('OpenStreetMap').setView([41.87, -87.62], 6)
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(renameThisMapObject)
+var Map3 = L.map('Map3').setView([41.87, -87.62], 6)
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(worldcanvas)
+var statesUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(statesUrl, function (data) {
  var stateStyle = function (feature) {
-   var pop = feature.properties.POPULATION
+   var age = feature.properties.MED_AGE
    var statecolor = 'olive'
    if (age < 38) { statecolor = 'green'}
    return {
@@ -16,7 +17,7 @@ var stateLayerOptions = {
   style: stateStyle,
   onEachFeature: createPopup
 }
-  L.geoJSON(data, { data, stateLayerOptions).addTo(OpenStreetMap)
+  L.geoJSON(data, { data, stateLayerOptions).addTo(Map3)
 })
 
 var createPopup = function (feature, layer) {}
