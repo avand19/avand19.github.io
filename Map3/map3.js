40d8ceb8ee3fc9ecaaa0fb44772bc25e151ff0d6
Aline M Vandenbold <avand19@lsu.edu>
2020-04-23 08:08:08 -0500

    Blank

--- Map3/map3.js
+++ Map3/map3.js
@@ -1,13 +1,6 @@
 /*gLobal L jQuery */
-var OpenStreetMap = L.map('map3').setView([41.87, -87.62], 6)
-var statesLayerObject = L.layerGroup('https://{s}.tile.opentopomap.org/{z}/{y}.png').addTo(OpenStreetMap)
-var grayBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(renameThisMapObject)
-var statesUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
-var streetsBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(renameThisMapObject)
-var basemapsObject = {
-  'Streets': streetsBasemapObject,
-  'Gray canvas': grayBasemapObject
-}
+var OpenStreetMap = L.map('OpenStreetMap').setView([41.87, -87.62], 6)
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(renameThisMapObject)
 jQuery.getJSON(statesUrl, function (data) {
  var stateStyle = function (feature) {
    var pop = feature.properties.POPULATION
@@ -19,8 +12,15 @@ jQuery.getJSON(statesUrl, function (data) {
    fillOpacity: 0.2
   }
 }
-  var geojsonOptions = {
-      style: stateStyle
+var stateLayerOptions = {
+  style: stateStyle,
+  onEachFeature: createPopup
 }
-  L.geoJSON(data, { data, geojsonOptions).addTo(OpenStreetMap)
+  L.geoJSON(data, { data, stateLayerOptions).addTo(OpenStreetMap)
 })
+
+var createPopup = function (feature, layer) {}
+  var name = feature.properties.STATE_NAME
+  var age = feature.properties.MED_AGE
+  layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+}
